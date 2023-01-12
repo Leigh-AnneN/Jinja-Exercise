3bd4f139eff7cd03539d8064b46a2e98a377663d
Leigh-Anne Niehaus <leigh-anneniehaus@Leigh-Annes-MacBook-Pro-2.local>
2023-01-11 19:09:00 -0800

    first commit challenge

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, request, render_template  
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app=Flask(__name__)
+app.config['SECRET_KEY']="whatever0710"
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def ask_questions():
+    """Generate and show form to ask words"""
+    # explain prompts?
+    prompts = story.prompts
+
+    return render_template("form.html", prompts = prompts)
+
+@app.route('/story')
+def show_story():
+    """Show story result"""
+    #?explain the request.args?
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
